fbc15c71471e55ad693416c3075f985e13fd6274
Robin Breen <breen@ecohealthalliance.org>
2023-04-27 09:43:13 -0400

    created new extent for HFI

--- R/get_extents.R
+++ R/get_extents.R
@@ -11,21 +11,32 @@
 
 
 ### CAUCASUS (ARM + AZE +GEO) ###
+### using CRS from caucasus_provinces
 get_caucasus_extent <- function(){
-  # Identify extent of South Caucasus region (done on next line)
+  # 1.) Identify extent of South Caucasus region (done on next line)
   # terra::ext(caucasus_provinces)
-  # Create extent based on slightly larger extent then caucasus_provinces
+  # 2.) Create extent based on slightly larger extent then caucasus_provinces
   extent_cauc <- terra::ext(39.6, 50.7, 38, 44)
   return(extent_cauc)
 }
 
 
+### CAUCASUS (ARM + AZE +GEO) ###
+### using CRS from caucasus_provinces_HFI (extent in meters)
+get_caucasus_extent_HFI <- function(){
+  # 1.) Identify extent of South Caucasus region (done on next line)
+  # terra::crs((terra::rast(HFI_2000_data)), proj = TRUE)
+  # 2.) Since CRS is different (in meters, not degrees), create a new extent slightly larger than the caucasus region
+  extent_cauc_hfi <- terra::ext(3250000, 4350000, 4550000, 5250000)
+  return(extent_cauc_hfi)
+}
+  
 
 ### ARMENIA ###
 get_armenia_extent <- function(){
-  # Identify extent of Armenia (done on next line)
+  # 1.) Identify extent of Armenia (done on next line)
   # terra::ext(armenia_provinces)
-  # Create extent based on slightly larger extent then armenia_provinces
+  # 2.) Create extent based on slightly larger extent then armenia_provinces
   extent_arm <- terra::ext(43.3, 46.7, 38.8, 41.35)
   return(extent_arm)
 }
@@ -33,9 +44,9 @@ get_armenia_extent <- function(){
 
 ### AZERBAIJAN ###
 get_azerbaijan_extent <- function(){
-  # Identify extent of Azerbaijan (done on next line)
+  # 1.) Identify extent of Azerbaijan (done on next line)
   # terra::ext(azerbaijan_provinces)
-  # Create extent based on slightly larger extent then azerbaijan_provinces
+  # 2.) Create extent based on slightly larger extent then azerbaijan_provinces
   extent_aze <- terra::ext(44.7, 50.7, 38.3, 41.95)
   return(extent_aze)
 }
@@ -43,9 +54,9 @@ get_azerbaijan_extent <- function(){
 
 ### GEORGIA ###
 get_georgia_extent <- function(){
-  # Identify extent of Georgia (done on next line)
+  # 1.) Identify extent of Georgia (done on next line)
   # terra::ext(georgia_provinces)
-  # Create extent based on slightly larger extent then georgia_provinces
+  # 2.) Create extent based on slightly larger extent then georgia_provinces
   extent_geo <- terra::ext(39.2, 46.75, 40.5, 43.9)
   return(extent_geo)
 }
