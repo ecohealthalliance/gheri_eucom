39114e22007460e1efc05b8298e3a4fd79532b66
Robin Breen <breen@ecohealthalliance.org>
2023-05-01 14:52:14 -0400

    code out unnecessary code for now

--- R/processing_mammal_data.R
+++ R/processing_mammal_data.R
@@ -12,7 +12,7 @@
 #' 
 c
 
-### Code to process mammal data that needs to be:
+### Overall goal of editing and processing the mammal data that needs to be:
 # 1.) debugged
 # 2.) turned into functions
 # 3.) incorporated into _targets.R file
@@ -37,14 +37,14 @@ mammal_data_masked <- st_intersection(mammal_data_crop_sf, caucasus_provinces_sf
 
 # 2.) generate a 1/X (e.g.1/10) degree resolution world map of mammalian biodiversity by rasterizing all the layers.
 # mammal_raster <- raster(mammal_shapes, res = 1/10)      # Noam's example code
-mammal_raster2 <- raster(mammal_data_masked, res = 1/10)  # My code
+# mammal_raster2 <- raster(mammal_data_masked, res = 1/10)  # My code: where code breakdown occurs
 
 # 3.) fasterize mammal rasters
 # mammal_sum <- fasterize(mammal_shapes, mammal_raster, fun="sum")         # Noam's example code
-mammal_sum2 <- fasterize(mammal_data_masked, mammal_raster2, fun="sum", )  # My code (where to code breaks down)
+#mammal_sum2 <- fasterize(mammal_data_masked, mammal_raster2, fun="sum", )  # My code (where to code breaks down)
 
 # 4.) Adjust margins around plot (not necessary, but can use if desired)
-par(mar=c(0,0.5,0,0.5))
+#par(mar=c(0,0.5,0,0.5))
 # plot(mammal_sum, axes=FALSE, box=FALSE)  # Noam's example code
 
 
