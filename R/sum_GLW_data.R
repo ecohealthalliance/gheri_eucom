c0904b501111b7b44eb746190c64dd6b46ce8df2
Robin Breen <breen@ecohealthalliance.org>
2023-05-01 14:53:07 -0400

    code out unnecessary code temporarily

--- R/sum_GLW_data.R
+++ R/sum_GLW_data.R
@@ -18,18 +18,18 @@ sum_GLW_data <- function(){
 # 1.) Loop to unpack GLW packed rasters?
 
 # 2.) create a list of all the spatraster files
-spatrasters <- list.files(pattern = "_caucasus_GLW$", full.names = TRUE)
+#spatrasters <- list.files(pattern = "_caucasus_GLW$", full.names = TRUE)
 
 # 3.) load all the spatrasters into a SpatRasterList
-spatraster_list <- terra::rast(list_packspatrasters)
+#spatraster_list <- terra::rast(list_packspatrasters)
 
 # 4.) create a new spatraster with a variable "sum_livestock"
-sum_livestock <- sum(spatraster_list)
+#sum_livestock <- sum(spatraster_list)
 
 # 5.) set the variable name to "sum_livestock"
-names(sum_livestock) <- "sum_livestock"
+#names(sum_livestock) <- "sum_livestock"
 
 # 6.) save the new spatraster as a GeoTIFF file
-writeRaster(sum_livestock, "sum_livestock.tif", format = "GTiff", overwrite = TRUE)
+#writeRaster(sum_livestock, "sum_livestock.tif", format = "GTiff", overwrite = TRUE)
 
 
