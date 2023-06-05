a78e849349ba957e1601c5f20e69dcc83ddb9815
Robin Breen <breen@ecohealthalliance.org>
2023-06-05 17:14:58 -0400

    added country-level plots

--- R/plot_livestock.R
+++ R/plot_livestock.R
@@ -1,26 +1,26 @@
-#' Function to plot GLW livestock data
+#' Functions to plot GLW livestock data
 #'
 #'
 #' @title plot_livestock
 
-#' @param summed_livestock aggregation of GLW data file as packedspatraster
+#' @param summed_livestock_caucasus aggregation of GLW data file as packedspatraster
 #' @param armenia_provinces sf object to provide outline of admin1 boundries for Armenia
 #' @param armenia_provinces sf object to provide outline of admin1 boundries for Azerbaijan
 #' @param georgia_provinces sf object to provide outline of admin1 boundries for Georgia
 #'
 #' 
 #' @examples
-#' plot_livestock(summed_livestock, armenia_provinces, azerbaijan_provinces, georgia_provinces)
+#' plot_livestock_caucasus(summed_livestock, armenia_provinces, azerbaijan_provinces, georgia_provinces)
 #' 
 #' 
 
-plot_livestock <- function(summed_livestock, armenia_provinces, azerbaijan_provinces, georgia_provinces){
+plot_livestock_caucasus <- function(summed_livestock_caucasus, armenia_provinces, azerbaijan_provinces, georgia_provinces){
 
   # rast to unpack packedspatraster
-summed_livestock_rast <- terra::rast(summed_livestock)
+  summed_livestock_caucasus_rast <- terra::rast(summed_livestock_caucasus)
   
   # plot GLW + outline of caucasus provinces
-  terra::plot(summed_livestock_rast,
+  terra::plot(summed_livestock_caucasus_rast,
               axes = FALSE,
               smooth = TRUE,
               plg=list(
@@ -49,5 +49,28 @@ Azerbaijan",    x=49, y= 41.5, cex = 0.7, bty="n")
   terra::plot(georgia_provinces$geometry,
               add = TRUE)
 }
+#plot_livestock_caucasus(summed_livestock_caucasus, armenia_provinces, azerbaijan_provinces, georgia_provinces)
 
-# plot_livestock(summed_livestock, armenia_provinces, azerbaijan_provinces, georgia_provinces)
+
+plot_livestock_country <- function(summed_livestock_country, country_provinces){
+# rast to unpack packedspatraster
+  summed_livestock_country_rast <- terra::rast(summed_livestock_country)
+
+# plot GLW + outline of caucasus provinces
+terra::plot(summed_livestock_country_rast,
+            axes = FALSE,
+            smooth = TRUE,
+            buffer = TRUE,
+            plg=list(
+              title = "   Number of
+ livestock",
+              title.cex=0.8,
+              size = 0.7,
+              loc = "right"))
+
+terra::plot(country_provinces$geometry,
+            add = TRUE)
+}
+#plot_livestock_country(summed_livestock_georgia, georgia_provinces)
+#plot_livestock_country(summed_livestock_armenia, armenia_provinces)
+#plot_livestock_country(summed_livestock_azerbaijan, azerbaijan_provinces)
