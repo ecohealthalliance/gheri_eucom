8a2a9374efd3ddb78016887afa02cd219eb0a2b1
Robin Breen <breen@ecohealthalliance.org>
2023-06-09 13:57:15 -0400

    added legend

--- R/plot_landcover.R
+++ R/plot_landcover.R
@@ -11,18 +11,16 @@
 
 # Ideally get ggplot once some issues sorted ot, but for now, just doing a terra::plot
 
-output_landcover <- crop_landcover(world_land_cover_vrt, western_asia_crop)
+#output_landcover <- crop_landcover(world_land_cover_vrt, western_asia_crop)
 
+plot_landcover <- function(output_landcover, western_asia_crop){
 #Add white box to put legend over
 # Create a matrix with the coordinates of the box vertices
 coords <- matrix(c(40.17, 38.2,
-                   44, 38.2,
-                   44, 40,
+                   43.2, 38.2,
+                   43.2, 40,
                    40.17, 40,
                    40.17, 38.2), ncol = 2, byrow = TRUE)
-#white_box <- polygon(coords, col='white', border='white')
-
-
 
 #Plot
 terra::plot(output_landcover,
@@ -32,15 +30,16 @@ terra::plot(western_asia_crop$geometry,
             add = TRUE)
 add_legend(legend = "Regions of
 Georgia",    x=39.5, y= 42.6, pch = 15, col="#99cc99", cex = 0.9, bty="n")
-white_box <- polygon(coords, col='white', border='white')
+box <- polygon(coords,  col = alpha("gray", 0.8), border='gray')
+add_legend(legend = "Tree cover",         x=40.12, y= 40.1, pch = 15, col="darkgreen", cex = 0.9, bty="n")
+add_legend(legend = "Grassland",          x=40.12, y= 39.85, pch = 15, col="yellow", cex = 0.9, bty="n")
+add_legend(legend = "Cropland",           x=40.12, y= 39.6, pch = 15, col="magenta", cex = 0.9, bty="n")
+add_legend(legend = "Built-up",           x=40.12, y= 39.35, pch = 15, col="red", cex = 0.9, bty="n")
+add_legend(legend = "Water",              x=40.12, y= 39.1, pch = 15, col="blue", cex = 0.9, bty="n")
+add_legend(legend = "Sparse vegetation",  x=40.12, y= 38.85, pch = 15, col="darkgray", cex = 0.9, bty="n")
+add_legend(legend = "Snow",               x=40.12, y= 38.6, pch = 15, col="white", cex = 0.9, bty="n")
 
-
-
-add_legend(legend = "health clinics",    x=25.1, y= -21, pch = 15, col="#99cc99", cex = 1, bty="n")
-add_legend(legend = "hospitals",         x=25.1, y= -21.4, pch = 15, col="#ff8080", cex = 1, bty="n")
-add_legend(legend = "central hospitals", x=25.1, y= -21.8, pch = 15, col="#ffff00", cex = 1, bty="n")
-
-
+}
 
 #plot_mammal_rich_terraplot <- function(mammal_rich, armenia_provinces, azerbaijan_provinces, georgia_provinces){
 # rast to unpack packedspatraster
